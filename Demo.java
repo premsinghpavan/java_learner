9b68bcf2888822648dacef763b1e7b46b004e09d
pavan <pavansingh75prem@gmail.com>
2017-07-22 14:44:13 -0500

    Demo program

---
+++ Demo.java
@@ -0,0 +1,13 @@
+class Demo
+{
+public static void main(String args[])
+{
+int num=-5;
+if (num==0)
+System.out.println("It is zero");
+else if(num>0)
+System.out.println("It is positive");
+else
+System.out.println("it is negative");
+}
+}
